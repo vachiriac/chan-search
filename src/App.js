42331e3af20da9082e203560da876ad47dfed7fa
zapacitu18 <irinam200320@gmail.com>
2021-07-15 13:25:34 +0300

    Remove white background when quotes are displayed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ function App() {
 	return (
 		<div className="App flex flex-col bg-gray-100 h-screen">
 			<h1 className=" text-7xl mt-32">Chan Search</h1>
-			<h2 className="mt-6 text-2xl">Search your favourite anime character quotes!</h2>
+			<h2 className="mt-6 text-2xl">
+				Search your favourite anime character quotes!
+			</h2>
 			<div className="pt-16">
 				<Search />
 			</div>
-			<h3 className="p-6">Made with 💗 by <a href="https://github.com/zapacitu18/">Chiriac Andrei</a></h3>
+			<div className="bg-gray-100">
+				<h3 className="p-6">
+					Made with 💗 by{' '}
+					<a href="https://github.com/zapacitu18/">Chiriac Andrei</a>
+				</h3>
+			</div>
 		</div>
 	);
 }
